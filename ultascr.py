9d53ac241e0c1e7c498d49df8a4414ae4ae05dd4
eeghor <eeghor@gmail.com>
2016-12-07 08:34:28 +1100

    using xpath

--- ultascr.py
+++ ultascr.py
@@ -20,7 +20,22 @@ driver.get(start_page_url)
 #driver.implicitly_wait(20)
 # wait up to 10 seconds before throwing a TimeoutException (if there's still no "Previous" link)
 
+
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, "2005-06"))).click()
 
-for tr in driver.find_elements_by_class_name("even"):
-	print(tr.text)
+# get the table headers that are in <tr> that belongs to the class "datatrheader"
+hdrs = driver.find_element_by_class_name("datatrheader").text.split()
+
+# table
+tbl = driver.find_element_by_id("tablesort")
+# print(tbl)
+# got to the table first
+rows = tbl.find_elements_by_xpath('//tbody/tr[@class]')
+
+for r in rows:
+	print(r.text)
+# collect all rows
+#rows = driver.find_elemelts_by_xpath()
+
+# for tr in driver.find_elements_by_class_name("even"):
+# 	print(tr.text)
